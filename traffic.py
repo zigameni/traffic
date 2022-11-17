e003299f1d592c0a422ba6edd7b74c232a983754
zigameni <shehu.meni@gmail.com>
2022-11-17 19:00:11 +0100

    Implemented the function get_model(), it returns the model after it is trained.

--- traffic.py
+++ traffic.py
@@ -58,33 +58,33 @@ def load_data(data_dir):
     be a list of integer labels, representing the categories for each of the
     corresponding `images`.
     """
-    
+
     images = []
     labels = []
-    
-    # getting the root directory of all the data. 
-    root = os.path.join(os.getcwd(), data_dir) 
-    
-    # getting the list of all the subdirs within the data folder, 
+
+    # getting the root directory of all the data.
+    root = os.path.join(os.getcwd(), data_dir)
+
+    # getting the list of all the subdirs within the data folder,
     # each is a label for our model
     subdirs = sorted([int(i) for i in os.listdir(root)])
 
     for subdir in subdirs:
         for filename in os.listdir(os.path.join(root, str(subdir))):
             # loading the ima file using the opencv module
-            #image path, we dont need to seperate this, but it has batter readability
+            # image path, we dont need to seperate this, but it has batter readability
             imagepath = os.path.join(root, str(subdir), filename)
-            img = cv2.imread(imagepath) 
+            img = cv2.imread(imagepath)
 
             # resizing the image matrix to make it suitable for reading into the nural net
-            resized_img = cv2.resize(img, (IMG_WIDTH, IMG_HEIGHT), interpolation = cv2.INTER_AREA)
+            resized_img = cv2.resize(
+                img, (IMG_WIDTH, IMG_HEIGHT), interpolation=cv2.INTER_AREA)
 
-            # store image into list. 
+            # store image into list.
             images.append(resized_img)
-            
-            # each subdir is the name of the sign 
-            labels.append(subdir)
 
+            # each subdir is the name of the sign
+            labels.append(subdir)
 
     return (images, labels)
 
@@ -95,7 +95,53 @@ def get_model():
     `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
     The output layer should have `NUM_CATEGORIES` units, one for each category.
     """
-    raise NotImplementedError
+
+    model = tf.keras.models.Sequential([
+        # Convolutional layer. Learns 32 filters using 3x3 kernel
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', 
+        input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)), 
+
+        # max-pooling layer, using 2x2 pool size
+        tf.keras.layers.MaxPool2D(pool_size=(2,2)), 
+
+        # add droupout to remove 20% random neurons from the net on every itration 
+        tf.keras.layers.Dropout(0.2), 
+        
+        # Convolutional layer, Learns 64 filters using 3x3 kernel
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu'), 
+
+        # Max-pooling layer, using 2x2
+        tf.keras.layers.MaxPooling2D(pool_size=(2,2)),
+
+        # adding dropout to remove 20% neurons from the net by iteration
+        tf.keras.layers.Dropout(0.2),
+
+        # flatten units
+        tf.keras.layers.Flatten(),
+
+        # adding hidden layer with 128 neurons
+        tf.keras.layers.Dense(128, activation='relu'),
+
+        # to avoid overfitting we add anouth drop out
+        tf.keras.layers.Dropout(0.2),
+
+        # Adding an output layer with N neurons for all categories 
+        tf.keras.layers.Dense(NUM_CATEGORIES, activation='softmax')
+
+    ])
+    
+
+    # traning the neural net 
+    model.compile(
+        optimizer = 'adam', 
+        loss = 'categorical_crossentropy',
+        metrics = ['accuracy']
+    )
+
+    return model
+
+
+    
 
 
 if __name__ == "__main__":
